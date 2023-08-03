0ad9c5444a2aa50c7ac40463e1e06be2abcb4c47
Necrofantasia <n3cr0f4nt4s14@gmail.com>
2023-08-03 20:31:33 +0200

    Create Validation.ts

---
+++ src/util/validation/Validation.ts
@@ -0,0 +1,27 @@
+/**
+ * File: Validation.ts
+ * 
+ * @author Necrofantasia
+ * @created 2023.08.02
+ * @updated 2023.08.03
+ */
+
+/**
+ * The Validation class.
+ * 
+ * @author Necrofantasia
+ * @created 2023.08.02
+ * @updated 2023.08.03
+ */
+class Validation {
+
+    /** This is a utility class. It contains only static methods and is non instantiable. */
+    private constructor() {
+        //TODO: Improve error: UnsupportedOperationException.
+        throw new Error();
+    }
+}
+
+export {
+    Validation
+};

5cdc943e452db5caf51c623df4623a2b344d30d8
Necrofantasia <n3cr0f4nt4s14@gmail.com>
2023-08-05 03:22:18 +0200

    Create ValidationTypes.ts

---
+++ src/util/validation/ValidationTypes.ts
@@ -0,0 +1,28 @@
+/**
+ * File: ValidationTypes.ts
+ * 
+ * @author Necrofantasia
+ * @created 2023.08.05
+ * @updated 2023.08.05
+ */
+
+/**
+ * The ValidationTypes enum.
+ * 
+ * @author Necrofantasia
+ * @created 2023.08.05
+ * @updated 2023.08.05
+ */
+enum ValidationTypes {
+	BIGINT = "bigint",
+	BOOLEAN = "boolean",
+	FUNCTION = "function",
+	NUMBER = "number",
+	OBJECT = "object",
+	STRING = "string",
+	SYMBOL = "symbol"
+}
+
+export {
+    ValidationTypes
+};

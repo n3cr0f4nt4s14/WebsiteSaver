36d294e71c9fa2c2d4df2a73156ba3a0f8c4a2b7
Necrofantasia <n3cr0f4nt4s14@gmail.com>
2023-08-05 03:25:32 +0200

    Create Internal.ts

---
+++ src/util/validation/Internal.ts
@@ -0,0 +1,18 @@
+import { Validation } from "./Validation";
+import { ValidationTypes } from "./ValidationTypes";
+
+/**
+ * This file serves as a collection point for all files in the `validation` package.
+ * 
+ * **Always use this file when using objects of the `validation` package, do not use the
+ * validation files directly.**
+ * 
+ * @authors Valentin Schmalz
+ * @created 2023.08.05
+ * @updated 2023.08.05
+ */
+
+export {
+	Validation,
+	ValidationTypes
+};
